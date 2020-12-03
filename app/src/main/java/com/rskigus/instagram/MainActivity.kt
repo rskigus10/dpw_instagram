5f18b3d09bf97d0f97dd4421a4e5ebfc775905b1
rskigus10 <71111285+rskigus10@users.noreply.github.com>
2020-12-03 20:49:58 +0700

    instagram_MP

--- app/src/main/java/com/rskigus/instagram/MainActivity.kt
+++ app/src/main/java/com/rskigus/instagram/MainActivity.kt
@@ -2,10 +2,22 @@ package com.rskigus.instagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lbl_follower_count = findViewById<TextView>(R.id.lbl_follower_count)
+        val btn_follow = findViewById<Button>(R.id.btn_follow)
+        var follow_count = 900
+
+        lbl_follower_count.text = follow_count.toString()
+        btn_follow.setOnClickListener {
+            follow_count++
+            lbl_follower_count.text = follow_count.toString()
+        }
     }
 }
